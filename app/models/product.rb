2be19561476902dff48a8cd816fe58a969605d0a
PierreOssun <pierre.giraud@mailfence.com>
2018-03-05 15:15:56 +0100

    Added more test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
-  
+
   validates :title, presence: true, length: { maximum: 255 }
   validates :description, presence: true, length: { in: 4..3000 }
-  validates :price, presence:true, numericality: true
+  validates :price, presence:true, :numericality => {:greater_than_or_equal_to => 0}
   validates :image_url, presence:true
 
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,8 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  context 'validation' do
+  it "should validate title length" do
+    product = Product.new(title:'', description: "asvdfwrefvwqregq", price: 10.58, image_url: "http://www.googleimages?image.png")
+    expect(product.valid?).to be false
+  end
+
+  it 'should validate description length' do
+    product = Product.new(title:'frwefwerf', description: "asv", price: 10.58, image_url: "http://www.googleimages?image.png")
+    expect(product.valid?).to be false
+  end
+
+  it 'should validate price' do
+    product = Product.new(title:'frwefwerf', description: "asfev", price: "fe", image_url: "http://www.googleimages?image.png")
+    expect(product.valid?).to be false
+  end
+
+
+  it 'should validate title presence' do
+    product = Product.new(description: "asv", price: 10.58, image_url: "http://www.googleimages?image.png")
+    expect(product.valid?).to be false
+  end
+
+  it 'should validate description presence' do
+    product = Product.new(title:'frwefwerf', price: 10.58, image_url: "http://www.googleimages?image.png")
+    expect(product.valid?).to be false
+  end
+
+  it 'should validate price presence' do
+    product = Product.new(title:'frwefwerf', description: "asv", image_url: "http://www.googleimages?image.png")
+    expect(product.valid?).to be false
+  end
+
+  it 'should validate image_url presence' do
+    product = Product.new(title:'frwefwerf', description: "asfev", price: "fe")
+    expect(product.valid?).to be false
+  end
+
+  it 'should validate price presence' do
+    product = Product.new(title:'frwefwerf', description: "asv",  price: -1, image_url: "http://www.googleimages?image.png")
+    expect(product.valid?).to be false
+  end
+
   it "should persist a post" do
   Product.create(title:"gqergbqreg", description: "asvdfwrefvwqregq", price: 10.58, image_url: "http://www.googleimages?image.png")
   expect(Product.count).to eq(1)
+end
 end
 end
