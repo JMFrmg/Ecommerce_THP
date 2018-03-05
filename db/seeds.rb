915a3b74b2a44fc788a5eadfd57db073b58798be
PierreOssun <pierre.giraud@mailfence.com>
2018-03-05 15:31:14 +0100

    seed the seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@ User.create(email:"jean.bernard@gmail.com", username:"jeanbernard", password:"je
 User.create(email:"alfred.duverger@gmail.com", username:"alfred", password:"alfredduverger", password_confirmation:"alfredduverger")
 User.create(email:"xavier.leboucher@gmail.com", username:"xavier", password:"xavierleboucher", password_confirmation:"xavierleboucher")
 User.create(email:"edouard.lachaise@gmail.com", username:"edouard", password:"edouardlachaise", password_confirmation:"edouardlachaise")
+Product.create(title:"Chat obese siamois", description: "Chat obese de premiere classe", price: 5.99, image_url: "http://www.googleimages/image1.png")
+Product.create(title:"Chat obese maine coon", description: "Chat obese de premiere main", price: 10, image_url: "http://www.googleimages/image2.png")
+Product.create(title:"Chat obese egyptien", description: "Chat obese de premiere instance", price: 12.99, image_url: "http://www.googleimages/image3.png")
+Product.create(title:"Chat obese bleu", description: "Chat obese avocat", price: 12.99, image_url: "http://www.googleimages/image4.png")
