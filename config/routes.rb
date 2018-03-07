92efea2066cc2fd3dcae90598b880691dc84ddc2
Matthieu <matthieu.colombert@gmail.com>
2018-03-07 12:46:31 +0100

    add show and orders_index html files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   
-  get 'orders/index'
+ 
 
-  get 'orders/show'
+  root 'home#index'
+
+  resources :orders
+
+  resources :products
 
   get 'carts/show'
   get 'carts/destroy'
@@ -11,7 +15,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   
-  root 'home#index'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
