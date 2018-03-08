700e1a54d012ed1159094a9d28d17837b58a36d0
PierreOssun <pierre.giraud@mailfence.com>
2018-03-08 14:55:43 +0100

    indexdesproduits check

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,13 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = Product.paginate(:page => params[:page], :per_page => 5)
+  end
+
   def show
   	@product = Product.find(params[:id])
   end
 
-   
+
 
 end
