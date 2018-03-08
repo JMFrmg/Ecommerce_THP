f342b8fa4e5ebe1a9fc0bafe8e15e49a2586835e
Matthieu <matthieu.colombert@gmail.com>
2018-03-08 16:40:58 +0100

    mailing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
     @products = products
 
     mail(
-    	delivery_method_options: { api_key: '2f48b7b5331c5aaae12ea4e8b073c22f', secret_key: 'f20ab8630b031fc7fbd7ea6b64fc93e1', port: '25' },
+    	delivery_method_options: { api_key: '', secret_key: '', port: '25' },
     	from: "edouard.dumuguet@gmx.fr",
     	to: @user.email,
     	subject: 'Détails de votre commande')
