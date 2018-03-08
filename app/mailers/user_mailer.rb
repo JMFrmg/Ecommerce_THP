b37bbffb24b8a2563664292e05371f6affe74e5a
Matthieu <matthieu.colombert@gmail.com>
2018-03-08 18:05:25 +0100

    mail to admins

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,21 @@ class UserMailer < ApplicationMailer
 		mail(
     		delivery_method_options: { api_key: ENV['MAILJET_API_KEY'], secret_key: ENV['MAILJET_API_SECRET'], port: '25' },
     		from: "edouard.dumuguet@gmx.fr",
-    		to: @user.email,
+    		to: @user.email, 
     		subject: 'Détails de votre commande')
-  end
+    end
+
+    def emails_to_admins(user, products)
+    	Dotenv.load
+    	@products = products
+    	@user = user
+    	@admins = ['piere@live.fr', 'a.lotz@hotmail.fr', 'tina.helfer2@gmail.com', 'matthieu.colombert@gmail.com']
+    	@admins.each do |email|
+			mail(
+    			delivery_method_options: { api_key: ENV['MAILJET_API_KEY'], secret_key: ENV['MAILJET_API_SECRET'], port: '25' },
+    			from: "edouard.dumuguet@gmx.fr",
+    			to: email, 
+    			subject: 'Détails de commande')
+		end
+	end
 end
